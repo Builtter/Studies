3b116a89da0de65cd2c0d52f237ce6631d7a41a9
Builtter <dego.dvl@gmail.com>
2023-09-14 00:12:46 -0300

    13/09/2023 - Basico Js
    Aprendendo o básico

---
+++ Js Studies/fundamentos/basicStart.js
@@ -0,0 +1,7 @@
+// PI * raio * raio
+
+const pi = 3.141592;
+var raio = 10;
+var area = pi * raio ** 2; //potência em js utiliza-se **
+
+console.log("A área com base no raio de " + raio + " m é de " + area + "m²");
--- Js Studies/start.js
+++
@@ -1,1 +0,0 @@
-console.log('Estamos aqui!!')
