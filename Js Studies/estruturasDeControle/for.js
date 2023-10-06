d9d7f3d1a602ac2cfcb0bbbc0e2b11179ba7d71b
Builtter <dego.dvl@gmail.com>
2023-10-05 23:20:31 -0300

    05/10/23
    continuar na aula 87. Usando Break/Continue
    
    Fiz somente uma aula pra n deixar passar

--- Js Studies/estruturasDeControle/for.js
+++ Js Studies/estruturasDeControle/for.js
@@ -15,6 +15,26 @@ console.log("---------------------- FOR 2 --------------------");
 
 let notas = [6.5, 5.8, 9.8, 9.9, 0.9];
 
-for (i = 0; i < notas.length; i++) {
+for (let i = 0; i < notas.length; i++) {
     console.log(`Nota = ${notas[i]}`)
+}
+
+
+console.log("---------------------- FOR 3 --------------------");
+// FOR IN
+
+for (let i in notas) {
+    console.log(`Aqui temos o indicie: ${i}  e nota que corresponde ao incidie dentro do arrey: ${notas[i]}`)
+}
+
+const pessoa = {
+    nome: 'Pedro',
+    sobrenome: 'Augusto',
+    idade: 57,
+    peso: 75,
+    endereco: 'Rua caninana Qd 3587, numero 25'
+}
+
+for (let atributo in pessoa) {
+    console.log(`${atributo} = ${pessoa[atributo]}`)
 }
