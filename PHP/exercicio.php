0cebb69f60165b02176e8c20485ebde16f57bf7a
Builtter <dego.dvl@gmail.com>
2023-10-20 17:38:37 -0300

    20/10/2023

---
+++ PHP/exercicio.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Exercicio</title>
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+
+</head>
+
+<body class="exercicio">
+
+    <header class="cabecalho">
+        <h1>Curso de PHP</h1>
+        <h2>Visualização de Exercicios</h2>
+    </header>
+
+    <nav class="navegacao">
+        <a href="#" class="verde"> Sem Formatação</a>
+        <a href="index.phtml" class="vermelho"> Voltar</a>
+    </nav>
+
+    <main class="principal">
+        <div class="conteudo">
+
+
+
+
+
+        </div>
+    </main>
+    <footer class="rodape">
+        TEI GAMES <?= date('Y');?>
+    </footer>
+
+</body>
+
+</html>
