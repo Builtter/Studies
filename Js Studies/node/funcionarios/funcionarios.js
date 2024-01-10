bd617efffda47d3221894b464217aa2af4885577
Builtter <dego.dvl@gmail.com>
2024-01-10 19:12:04 -0300

    10/01/2024
    continuar na aula 156

---
+++ Js Studies/node/funcionarios/funcionarios.js
@@ -0,0 +1,16 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
+const axiios = require('axios'); //client http - realiza requisições remotamente
+
+const pais = f => f.pais === 'China';
+const sexo = f => f.genero === 'F';
+const menorSalario = (funcio, funcioAtual) => {
+    return funcio.salario < funcioAtual.salario ? funcio : funcioAtual;
+};
+
+axiios.get(url).then(response => {
+    const funcionarios = response.data;
+
+    //desafio: Selecionar mulher, chinesa, menor salario
+    console.log(funcionarios.filter(pais).filter(sexo).reduce(menorSalario));
+
+})
