9faa14b987f139f469e208222bb60dde81ea0673
Builtter <dego.dvl@gmail.com>
2024-05-13 23:14:36 -0300

    Teste prova

--- Python/fac/teste.py
+++ Python/fac/teste.py
@@ -1,3 +1,21 @@
-import math
+from threading import Thread
 
-print(math.sqrt(36))
+minha_lista = []
+
+def funcao():
+    for i in range(100000):
+        minha_lista.append(1)
+    for i in range(100000):
+        minha_lista.pop()
+
+if __name__ == '__main__':
+    tarefas = []
+    for indice in range(10):
+        tarefa = Thread(target=funcao)
+        tarefas.append(tarefa)
+        tarefa.start()
+
+    print(len(minha_lista))
+    for tarefa in tarefas:
+        tarefa.join()
+    print(len(minha_lista))
