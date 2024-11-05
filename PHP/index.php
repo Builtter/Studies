8f9e7c9c936c38535467f047e4b40d8f77f920ff
Builtter <119297639+Builtter@users.noreply.github.com>
2024-11-04 21:29:46 -0300

    Tipos

--- PHP/index.php
+++ PHP/index.php
@@ -20,13 +20,19 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01 - Básico</h3>
+                    <h3>Básico</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
                         <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
                         <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=tipos&file=int">Tipo Interiro</a></li>
                     </ul>
                 </div>
             </nav>
---
+++ PHP/tipos/int.php
@@ -0,0 +1,20 @@
+<div class="titulo">Tipo inteiro</div>
+
+
+<?php
+
+echo 11,'<br>';
+
+var_dump(11);
+echo '<br>';
+
+echo -11,'<br>';
+
+echo 0x8BA12A . " hexadecimal",'<br>';//hexadecimal
+
+echo 012 . " octal",'<br>'; //octal
+
+echo 0b1101000101001 . " binaria",'<br>';//base binaria
+
+echo PHP_INT_MAX, '<br>'; /*valor máximo inteiro suportado pela arquitetura da máquina */
+echo PHP_INT_MIN, '<br>'; /*valor mínimo inteiro suportado pela arquitetura da máquina */
