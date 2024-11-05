0da36746e78aedc824c4c466bf3423c6946a456d
Builtter <119297639+Builtter@users.noreply.github.com>
2024-11-05 19:42:01 -0300

    Trocando comprimento padrão

--- PHP/index.php
+++ PHP/index.php
@@ -22,7 +22,7 @@
                 <div class="modulo verde">
                     <h3>Básico</h3>
                     <ul>
-                        <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=ola">Whats's Up PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
                         <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
                         <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
