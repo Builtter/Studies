ebf83316c8d6016229c1f73765bf744bb831e2a4
Builtter <119297639+Builtter@users.noreply.github.com>
2024-11-09 16:56:51 -0300

    Desafio

---
+++ PHP/variaveis/desafio_variaveis.php
@@ -0,0 +1,14 @@
+<div class="titulo">Desafio Variavies</div>
+
+<?php
+
+$a = "Nossa";
+$Nossa = "Eu";
+$Eu = "consegui";
+$consegui = "responder";
+$responder = "esse";
+$esse = "desafio";
+
+echo "Nossa Eu consegui responder esse desafio";
+echo "<br>";
+echo "$a! {$$a} {$$$a} {$$$$a} {$$$$$a} {$$$$$$a}.";
