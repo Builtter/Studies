dd9d75f0f99e15c33933b4a4932b577d51a7a9c0
Builtter <119297639+Builtter@users.noreply.github.com>
2024-11-18 21:15:04 -0300

    Formatando estrutura de modulos + nova estrutura de modulo 04. contole

--- PHP/index.php
+++ PHP/index.php
@@ -19,31 +19,15 @@
 
         <div class="conteudo">
             <nav class="modulos">
-                <div class="modulo verde">
-                    <h3>Básico</h3>
+                <div class="modulo roxo">
+                    <h3>04. Controle</h3>
                     <ul>
-                        <li><a href="exercicio.php?dir=basico&file=ola">Whats's Up PHP</a></li>
-                        <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
-                        <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
-                        <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
-                        <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
-                    </ul>
-                </div>
-                <div class="modulo vermelho">
-                    <h3>Tipos</h3>
-                    <ul>
-                        <li><a href="exercicio.php?dir=tipos&file=int">Tipo Interiro</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=float">Tipo Float</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=aritmeticas">Op. Aritmeticas</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio Precedência</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=string">Tipo String</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=desafio_string">Desafio String</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=boolean">Tipo Boolean</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=conversoes">Conversões</a></li>
+                        <li><a href="exercicio.php?dir=controle&file=if_else">If Else</a></li>
+
                     </ul>
                 </div>
                 <div class="modulo azul">
-                    <h3>Variáveis</h3>
+                    <h3>03. Variáveis</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=variaveis&file=basico">Váriaveis</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
@@ -55,12 +39,35 @@
                         <li><a href="exercicio.php?dir=variaveis&file=constantes">Constantes</a></li>
                     </ul>
                 </div>
+                <div class="modulo vermelho">
+                    <h3>02. Tipos</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=tipos&file=int">Tipo Interiro</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=float">Tipo Float</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=aritmeticas">Op. Aritmeticas</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio Precedência</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=string">Tipo String</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=desafio_string">Desafio String</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=boolean">Tipo Boolean</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=conversoes">Conversões</a></li>
+                    </ul>
+                </div>
+                <div class="modulo verde">
+                    <h3>01. Básico</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=basico&file=ola">Whats's Up PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+                    </ul>
+                </div>
             </nav>
         </div>
 
     </main>
     <footer class="rodape">
-        FORGING SITES <?= date('Y');?>
+        FORGING SITES <?= date('Y'); ?>
     </footer>
 
 </body>
