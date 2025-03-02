44f03722178c6608dab6f901db0829fc5b38be58
Builtter <119297639+Builtter@users.noreply.github.com>
2025-03-02 00:05:37 -0300

    Página de recursos

---
+++ PHP/PHP 7/repeticoes/recursos.php
@@ -0,0 +1,59 @@
+<div class="titulo">Recursos do Módulo 8</div>
+
+
+<div class="content">
+    <div class="bx-btn">
+        <a href="https://www.php.net/manual/pt_BR/language.control-structures.php">Control Structures</a>
+    </div>
+    <div class="bx-btn">
+        <a href="https://www.php.net/manual/pt_BR/control-structures.for.php">Control Structures</a>
+    </div>
+    <div class="bx-btn">
+        <a href="https://www.php.net/manual/pt_BR/control-structures.foreach.php">Control Structures Foreach</a>
+    </div>
+    <div class="bx-btn">
+        <a href="https://www.php.net/manual/pt_BR/control-structures.while.php">Control Structures While</a>
+    </div>
+    <div class="bx-btn">
+        <a href="https://www.php.net/manual/pt_BR/control-structures.do.while.php">Control Structures While</a>
+    </div>
+    <div class="bx-btn">
+        <a href="https://www.php.net/manual/pt_BR/control-structures.break.php">Control Structures Break</a>
+    </div>
+    <div class="bx-btn">
+        <a href="https://www.php.net/manual/pt_BR/control-structures.continue.php">Control Structures Continue</a>
+    </div>
+</div>
+
+
+<style>
+    .content{
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    .button-cont{
+        width: 300px;
+    }
+
+    .bx-btn{
+        border: 1px solid black;
+        background-color: #ccc;
+        border-radius: 20px;
+        padding-bottom: 5px;
+        margin-bottom: 5px;
+        width: 500px;
+    }
+
+    .bx-btn:hover{
+        background-color: #228B22;
+    }
+
+</style>
