c7e85b8b9af35544a50c45b660351008fba75c33
Builtter <dego.dvl@gmail.com>
2025-07-31 22:16:16 -0300

    Criando banco e ajustando conexão com instert de dados

--- PHP/PHP CFS/app/functions/db.php
+++ PHP/PHP CFS/app/functions/db.php
@@ -1,10 +1,16 @@
 <?php
 
 function connect() {
-    $pdo = new \PDO('mysql:host=localhost;dbname=cfs;charset=utf8','root','root');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    return $pdo;
+    try {
+        $pdo = new \PDO('mysql:host=localhost:3306;dbname=simp_db;charset=utf8','root','');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo 'Erro de conexão: ' . $e->getMessage();
+        exit;
+    }
+    
 }
 
 function create($table, $fields) {
@@ -12,13 +18,17 @@ function create($table, $fields) {
         $fields = (array) $fields;
     }
     
-    $sql = "INSERT INTO {$table}";
-    $sql .= "(" . implode(',', array_keys($fields)) . ")";
-    $sql .= " VALUES (" . ":v_" . implode(',:v_', array_keys($fields)) . ")";
-    
+    $sql = "INSERT INTO {$table} (" . implode(',', array_keys($fields)) . ") VALUES (:" . implode(', :', array_keys($fields)) . ")";
+
     $pdo = connect();
     $insert = $pdo->prepare($sql);
-    return $insert->execute($fields);
+
+    foreach ($fields as $key => $value) {
+        $insert->bindValue(":{$key}", $value);
+    }
+    
+    $insert->execute();
+    return $pdo;
 }
 
 function update() {
