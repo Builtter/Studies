352b1d381ed567b7cfe7a0ffebf28b0f5bd9baa3
Builtter <dego.dvl@gmail.com>
2025-09-04 01:19:34 -0300

    Aula 47 - Alterando dados de usuário

--- PHP/PHP CFS/public/pages/forms/update_user.php
+++ PHP/PHP CFS/public/pages/forms/update_user.php
@@ -6,7 +6,7 @@ $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 if(isEmpty()) {
     flash('message','Preencha todos os campos!');
-    return redirect('/update_user?id=' . $_POST['id']);
+    return redirect('edit_user&id=' . $id);
 }
 
 $validate = validate([
@@ -17,14 +17,13 @@ $validate = validate([
 
 $atualizado = update('users', $validate, ['ID_USER', $id]);
 
-dd($atualizado);
+if($atualizado) {
+    flash('message','Atualizado com sucesso!', 'success');
+    redirect('edit_user&id=' . $id);
 
-// if($atualizado) {
-//     flash('message','Atualizado com sucesso!', 'success');
-//     return redirect('/edit_user?id=' . $_POST['id']);
-// }else {
-//     flash('message','Erro ao Atualizar!');
-//     return redirect('/edit_user?id=' . $_POST['id']);
-// }
+}else {
+    flash('message','Erro ao Atualizar!');
+    redirect('edit_user&id=' . $id);
+}
 
 ?>
